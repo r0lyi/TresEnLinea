ee47a1d7dfb10a9a501c9568fdc1965ffda77b27
r0lyi <rsilvestre@elpuig.xeill.net>
2024-04-18 20:41:24 +0200

    Este el menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+menu();
+
+    }
+    public static void menu(){
+        String opcion ="";
+        switch (opcion){
+            case "Nueva Partida":
+                System.out.println("Estas en una partida");
+                break;
+            case "Cargar Partida":
+                System.out.println("Estas cargando la partida");
+                break;
+            case "Configuración":
+                System.out.println("Estas en configuración");
+                break;
+            case "salir":
+                System.out.println("Estas saliendo de la partida ");
+        }
+
     }
 }
