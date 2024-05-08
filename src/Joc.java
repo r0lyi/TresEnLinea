5b9938e092ba059f7b261c47a09e23da127b9b71
r0lyi <rsilvestre@elpuig.xeill.net>
2024-05-08 19:56:47 +0200

    Actualizado para el sprin 2

---
+++ src/Joc.java
@@ -0,0 +1,14 @@
+import jdk.jshell.spi.ExecutionControl;
+
+public class Joc {
+    private char tabla;
+    private int turno;
+
+    public static void nuevaPartida(){
+
+    }
+    public static void jugar(){
+    }
+    public static void jugadaGanadora(){
+    }
+}
